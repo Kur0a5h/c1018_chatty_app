c0628bdc01770b292fd6e7d039c9829df3e4df13
Kur0a5h <kuroash.esmaili@gmail.com>
2019-01-23 12:22:25 -0800

    Completed project setup

---
+++ src/components/nav.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+class Nav extends Component {
+    render(){
+        return (
+            <nav style={{padding:'0 8px'}} className="purple lighten-2 text-green">
+                <div className="nav-wrapper">
+                    <Link className="brand-logo" to="/">Chatty App</Link>
+
+                    <ul className="right">
+                    <li>
+                        <Link to="/">Home</Link>    
+                    </li>
+                    <li>
+                        <Link to="/chat">Chat</Link>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        )
+    }
+}
+export default Nav;
